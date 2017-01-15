3c7ddce571c6d9415f4ed4a066ba96f33457fe05
Matthew Gamble <git@matthewgamble.net>
2017-01-15 11:22:19 +1100

    Revert "Still trying to work out when systemd gets installed"
    This reverts commit 6028a6c64c3f2fc31c1fdca7089f02c9082ab89d.

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,15 +4,11 @@
 set -e
 
 source /root/functions.sh
-pacman -Qi systemd || true
 
 echo "Installing fake systemd package"
 aur_start
-pacman -Qi systemd || true
 aur_build systemd-dummy
-pacman -Qi systemd || true
 aur_finish
-pacman -Qi systemd || true
 
 # Install any packages
 source /root/phpfpm.sh
